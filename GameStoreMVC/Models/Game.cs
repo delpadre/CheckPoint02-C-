8bfdafcbed4a7db3bf10f9783ba2b4982b18aa00
Laboratório FIAP <labsfiap@fiap.com.br>
2026-04-27 22:03:50 -0300

    Add Model Game

--- GameStoreMVC/Models/Game.cs
+++ GameStoreMVC/Models/Game.cs
@@ -1,7 +1,33 @@
-﻿namespace GameStoreMVC.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GameStoreMVC.Models
 {
     public class Game
     {
+        public int Id { get; set; }
 
+        [Required(ErrorMessage = "Nome do jogo é obrigatório")]
+        [StringLength(150)]
+        [Display(Name = "Nome do Jogo")]
+        public string Nome { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Descrição é obrigatória")]
+        [Display(Name = "Descrição Curta")]
+        public string Descricao { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Preço é obrigatório")]
+        [Range(0.01, 99999.99, ErrorMessage = "Preço deve ser maior que zero")]
+        [Display(Name = "Preço (R$)")]
+        public decimal Preco { get; set; }
+
+        [Display(Name = "URL da Capa")]
+        public string? ImagemUrl { get; set; }
+
+        [Display(Name = "Categoria")]
+        public string Categoria { get; set; } = "Ação";
+
+        public bool EmDestaque { get; set; } = false;
+
+        public DateTime CriadoEm { get; set; } = DateTime.UtcNow;
     }
 }
