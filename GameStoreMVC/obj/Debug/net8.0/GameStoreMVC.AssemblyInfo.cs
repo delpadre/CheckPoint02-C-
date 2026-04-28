d88782313272fa95d63cc6077344e94f95c62adb
Giovanna Franco <giovannafgaudino@gmail.com>
2026-04-27 22:17:45 -0300

    add views

--- GameStoreMVC/obj/Debug/net8.0/GameStoreMVC.AssemblyInfo.cs
+++ GameStoreMVC/obj/Debug/net8.0/GameStoreMVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStoreMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9316f5036a08e29d369f9c986444f80bf2df8250")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7374c36de1616f620d981d855fbe602617cf7a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStoreMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStoreMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
